2cde105ac9243ea75c5127c484b7e10629ab020b
arapocket <arakazaryan@me.com>
2018-01-31 17:18:46 -0800

    installed camera

--- android/app/src/main/java/com/foxwatch/MainApplication.java
+++ android/app/src/main/java/com/foxwatch/MainApplication.java
@@ -3,6 +3,7 @@ package com.foxwatch;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
             new ReactVideoPackage(),
             new ReactNativePushNotificationPackage(),
             new RNDeviceInfo(),
