da8af9b8e1d673d7fd146d7d4780ddb0144e12d6
arapocket <arakazaryan@me.com>
2018-02-02 10:43:34 -0800

    installed up to maps, or android, might have missed some steps with the google maps api

--- android/app/src/main/java/com/foxwatch/MainApplication.java
+++ android/app/src/main/java/com/foxwatch/MainApplication.java
@@ -14,9 +14,11 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -36,7 +38,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RNDeviceInfo(),
             new RNSpinkitPackage(),
             new VectorIconsPackage(),
-            new RNBackgroundGeolocation()
+            new RNBackgroundGeolocation(),
+            new MapsPackage()
       );
     }
 
