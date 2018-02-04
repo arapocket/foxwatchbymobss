d99a83ea9f877ab6dbc155ad66f37b5a0c3974dd
arapocket <arakazaryan@me.com>
2018-02-03 17:20:09 -0800

    installed react native navigation for android

--- android/app/src/main/java/com/foxwatch/MainApplication.java
+++ android/app/src/main/java/com/foxwatch/MainApplication.java
@@ -15,12 +15,12 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -28,19 +28,21 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
+    public boolean isDebug() {
+      // Make sure you are using BuildConfig from your own application
+      return BuildConfig.DEBUG;
+    }
+
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RCTCameraPackage(),
-            new ReactVideoPackage(),
-            new ReactNativePushNotificationPackage(),
-            new RNDeviceInfo(),
-            new RNSpinkitPackage(),
-            new VectorIconsPackage(),
-            new RNBackgroundGeolocation(),
-            new MapsPackage()
-      );
+      return Arrays.<ReactPackage>asList(new RCTCameraPackage(), new ReactVideoPackage(),
+          new ReactNativePushNotificationPackage(), new RNDeviceInfo(), new RNSpinkitPackage(),
+          new VectorIconsPackage(), new RNBackgroundGeolocation(), new MapsPackage());
+    }
+
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+      return getPackages();
     }
 
     @Override
@@ -59,4 +61,10 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
+
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
+  }
+
 }
